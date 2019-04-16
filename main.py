3390c6e5195949b65e6f4c493d993ab0e6fcf245
Andrew Dixon <infinite_number@yahoo.com>
2019-04-15 19:56:44 -0500

    end of class: started template

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:MyNewPass@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(500))
+
+@app.route('/blog', methods=['POST', 'GET'])
+def blogs():
+    return render_template('blog.html')
+
+@app.route('/newpost', methods=['POST', 'GET'])
+    return render_template('newpost.html')
+
+if __name__ == '__main__':
+    app.run()
