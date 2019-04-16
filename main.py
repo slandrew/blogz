be1f16bedadb78cb603a482892c1d52aab930097
Andrew Dixon <infinite_number@yahoo.com>
2019-04-16 17:44:12 -0500

    Included error messages and database

--- main.py
+++ main.py
@@ -13,12 +13,39 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(500))
 
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
 @app.route('/blog', methods=['POST', 'GET'])
 def blogs():
-    return render_template('blog.html')
+
+    if request.method == 'POST':
+        blog_title = request.form['title']
+        blog_body = request.form['body']
+        new_blog = Blog(blog_title, blog_body)
+        title_error = ''
+        body_error = ''
+
+        if not blog_title:
+            title_error = 'Please include a title for your blog.'
+        if not blog_body:
+            body_error = 'Please enter a body for your blog.'
+
+        if not title_error and not body_error:
+            db.session.add(new_blog)
+            db.session.commit()
+            blogs = Blog.query.all()
+            return render_template('blog.html', title='Blogs', blogs=blogs)
+        else:
+            return render_template('newpost.html', title='New Post', blog_title=blog_title, blog_body=blog_body, title_error=title_error, body_error=body_error)
+    else:
+        blogs = Blog.query.all()
+        return render_template('blog.html', title='Blogs', blogs=blogs)
 
 @app.route('/newpost', methods=['POST', 'GET'])
-    return render_template('newpost.html')
+def new_post():
+    return render_template('newpost.html', title='New Post')
 
 if __name__ == '__main__':
     app.run()
