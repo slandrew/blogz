ef370a43dec0205a7df4e8f3a2206b4cdc958679
Andrew Dixon <infinite_number@yahoo.com>
2019-04-18 17:27:18 -0500

    did the setup and the steps up until signup

--- main.py
+++ main.py
@@ -18,24 +18,33 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(500))
     pub_date = db.Column(db.DateTime)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
         #checks to see if there is a time - may be redundant but I copied this from material
         if self.pub_date is None:
             pub_date = datetime.utcnow()
         self.pub_date = pub_date
+        self.owner = owner
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25))
+    password = db.Column(db.String(25))
+    blogs = db.relationship('Blog', backref='owner')
 
 @app.route('/blog', methods=['POST', 'GET'])
 def blogs():
 
+    blog_owner = User.query.filter_by(email=session['email']).first()
     #If Posted as new post
     if request.method == 'POST':
         #Collect form data, save to variables No commit yet to avoid erroneous database entries
         blog_title = request.form['title']
         blog_body = request.form['body']
-        new_blog = Blog(blog_title, blog_body)
+        new_blog = Blog(blog_title, blog_body, blog_owner)
         #Validation and Error
         title_error = ''
         body_error = ''
@@ -75,6 +84,55 @@ def blogs():
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
     return render_template('newpost.html', title='New Post')
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        if not email or not password:
+            flash('One or more fields are invalid.', 'error')
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['email'] = email
+            return redirect('/newpost')
+        else:
+            #TODO - response message
+            return '<h1> Duplicate User </h1>'
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            session['email'] = email
+            flash("Logged in")
+            return redirect('/newpost')
+        elif user and user.password != password:
+            flash('User password incorrect.', 'error')
+        elif not user:
+            flash('User does not exist.', 'error')
+
+
+    return render_template('login.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    return redirect('/blog')
 
 if __name__ == '__main__':
     app.run()
